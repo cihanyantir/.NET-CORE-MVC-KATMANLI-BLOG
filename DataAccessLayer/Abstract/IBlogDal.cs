a089b91dda3d66a8a826ed7c514bbe248bb4a1d2
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2021-11-25 23:45:34 +0300

    Syntax changed.

--- DataAccessLayer/Abstract/IBlogDal.cs
+++ DataAccessLayer/Abstract/IBlogDal.cs
@@ -9,10 +9,10 @@ namespace DataAccessLayer.Abstract
 {
     public interface IBlogDal
     {
-        List<Blog> ListAllCategory();
-        void AddCategory(Blog Blog);
-        void DeleteCategory(Blog Blog);
-        void UpdateCategory(Blog category);
+        List<Blog> ListAllBlog();
+        void AddBlog(Blog blog);
+        void DeleteBlog(Blog blog);
+        void UpdateBlog(Blog blog);
         Blog GetById(int id);
     }
 }
