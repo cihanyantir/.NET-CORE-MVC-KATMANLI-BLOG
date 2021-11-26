b9160d2dee680a7d2830e3ad84fc12834b716b2b
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2021-11-26 20:46:12 +0300

    Data Access Layer Abstracts definied.

--- DataAccessLayer/Abstract/IWriterDal.cs
+++ DataAccessLayer/Abstract/IWriterDal.cs
@@ -7,8 +7,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
-    public interface ICategoryDal:IGenericDal<Category>
+    interface IWriterDal:IGenericDal<Writer>
     {
-       
     }
 }
