815c9f9089552ee796a1200fddcd58fccf38b7da
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2022-06-20 22:50:42 +0300

    API migrations

---
+++ BlogAPI/Migrations/20220302171547_employee.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlogAPI.Migrations
+{
+    public partial class employee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+
+        //add-migration employee -Context Context
+
+    }
+}
