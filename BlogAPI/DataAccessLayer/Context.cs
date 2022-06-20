377e1564ffcedd16abdacf5f552c8a111311f1f6
cihanyantir <cihan.yantir@ogr.sakarya.edu.tr>
2022-06-20 22:48:23 +0300

    Context class

---
+++ BlogAPI/DataAccessLayer/Context.cs
@@ -0,0 +1,26 @@
+﻿using BlogAPI.DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogAPI.DataAccessLayer
+{
+    public class Context:DbContext
+    {
+        public Context(DbContextOptions<Context> options)  : base(options)
+        {
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)//bağlantı stringi
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-OE95DLD\\MSSQLSERVER01;database=CoreBlogAPIDbs; integrated security=true;");
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+
+    }
+}
